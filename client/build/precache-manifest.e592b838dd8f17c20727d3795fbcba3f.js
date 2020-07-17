8bdca773ac65f0ba97bc7ba55c9108dfe5e6aada
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-07-17 11:11:51 -0800

    first deployment test

---
+++ client/build/precache-manifest.e592b838dd8f17c20727d3795fbcba3f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a886de00791c8608dceccae3980c96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62186687d85f27317154",
+    "url": "/static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "ff358bbcd18030907207",
+    "url": "/static/js/2.4d15e837.chunk.js"
+  },
+  {
+    "revision": "50949285a495ff50d33a",
+    "url": "/static/js/3.ed3250a5.chunk.js"
+  },
+  {
+    "revision": "8ba510a098f2ac667385",
+    "url": "/static/js/4.03a9166f.chunk.js"
+  },
+  {
+    "revision": "f1f2b4df969b3d64da48",
+    "url": "/static/js/5.98b5501f.chunk.js"
+  },
+  {
+    "revision": "3ca9a3355f3a7dd0dbf0",
+    "url": "/static/js/6.b36a88a4.chunk.js"
+  },
+  {
+    "revision": "f575bc320038b30567ad",
+    "url": "/static/js/7.f2336e36.chunk.js"
+  },
+  {
+    "revision": "9942fb02c52aa692bbd2",
+    "url": "/static/js/8.6ea3c8c0.chunk.js"
+  },
+  {
+    "revision": "d8ed4a8a4c31e3d1b9bd",
+    "url": "/static/js/9.f9ed09f2.chunk.js"
+  },
+  {
+    "revision": "62186687d85f27317154",
+    "url": "/static/js/main.0b54dbf4.chunk.js"
+  },
+  {
+    "revision": "da23b5d85e90fbcd045b",
+    "url": "/static/js/runtime~main.afc49019.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
---
+++ client/src/components/complex/section/landing.footer/landing.footer.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Template = () => {
+  return (
+    <footer className="footer">
+      <ul className="nav">
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream home 1
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream home 2
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream home 3
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream home 4
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream home 5
+          </a>
+        </li>
+      </ul>
+      <p className="copyright">&copy; Copyright 2020 by aftoflBig5</p>
+    </footer>
+  );
+};
+
+export default Template;
