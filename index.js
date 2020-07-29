eefe471863f72a1e827f45b0b7f792c38a3b03ea
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-07-29 00:42:03 -0800

    Add Contentful Headless CMS successfully

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const { log } = console;
 const cors = require("cors");
 const enforce = require("express-sslify");
 
+const fs= require("fs");
+
 console.log('the initial NODE ENV IS : ', process.env.NODE_ENV);
 if (process.env.NODE_ENV !== 'production') dotenv.config({ path: './config.env' });
 
@@ -32,57 +34,63 @@ app.set("trust proxy", true);
 
 // //Cookie session -->
 // app.use(
-//     cookieSession({
-//         maxAge: 60 * 60 * 24 * 1000,
-//         keys: [keys.cookieKey]
-//     })
-//     );
-// app.use(passport.initialize());
-// app.use(passport.session());
-// <-- cookie session
-
-//Routing -->
-// require('./routes/authRoutes')(app);
-// require('./routes/billingRoutes')(app);
-// require('./routes/surveyRoutes')(app);
-// <-- Routing 
-// if (1>0) {
-if (process.env.NODE_ENV === 'production') {
-
-  console.log("WOW! I am in production :D ");
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  
-}
-
-app.use(express.static(path.join(__dirname, "client", "build")));
-// Express will serve up production assets and files like main.js & main.css
-
-app.get("/firebase-messaging-sw.js", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "client", "build", "firebase-messaging-sw.js")
-  );
-});
-// Express will serve up index.html if route isn't recognized
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-// app.configure("production", () => {
-//     // Express will serve up production assets and files like main.js & main.css
-//     app.use(express.static('client/build'));
-
+  //     cookieSession({
+    //         maxAge: 60 * 60 * 24 * 1000,
+    //         keys: [keys.cookieKey]
+    //     })
+    //     );
+    // app.use(passport.initialize());
+    // app.use(passport.session());
+    // <-- cookie session
+    
+    //Routing -->
+    // require('./routes/authRoutes')(app);
+    // require('./routes/billingRoutes')(app);
+    // require('./routes/surveyRoutes')(app);
+    // <-- Routing 
+    // if (1>0) {
+      if (process.env.NODE_ENV === 'production') {
+        
+        log("WOW! I am in production :D ");
+        app.use(enforce.HTTPS({ trustProtoHeader: true }));
+        
+      } else {
+        log('OMG! we are developing :-0 ');
+      }
+      
+      app.use(express.static(path.join(__dirname, "client", "build")));
+      // Express will serve up production assets and files like main.js & main.css
+      
+      app.get("/firebase-messaging-sw.js", (req, res) => {
+        res.sendFile(
+          path.join(__dirname, "client", "build", "firebase-messaging-sw.js")
+          );
+        });
+        // Express will serve up index.html if route isn't recognized
+        app.get("*", (req, res) => {
+          res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+        });
+        // app.configure("production", () => {
+          //     // Express will serve up production assets and files like main.js & main.css
+          //     app.use(express.static('client/build'));
+          
 //     // Express will serve up index.html if route isn't recognized
 //     const path = require('path');
 //     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
+  //         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  //     });
 // });
 
+fs.readFile("shaja.txt", (err, data) => {
+  log("text in file is :", data);
+});
+
 //setup port -->
-const port = process.env.PORT || 6070;
+const port = process.env.PORT || 5000;
 // app.listen(PORT);
 const server = app.listen(port, (error) => {
   if (error) throw error;
-  log(`Express server started running on port ${port}...`);
+  log(`Express server started on port ${port}...`);
 });
 // <-- setup port
 
